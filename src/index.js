63c4870d18cf0eac728996eab13a68b31ae5720b
Maryi Liliana Bustos <maryie098@hotmail.com>
2021-07-20 21:29:50 -0500

    Funcionalidades para interactuar con la vista HTML.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-import validator from './validator.js';
+import validator from "./validator.js";
 
-console.log(validator);
+let inputNombre = document.querySelector("#input_nombre");
+let inputNumero = document.querySelector("#input_numero");
+
+let divNombre = document.querySelector("#nombre");
+let divNumero = document.querySelector("#numero");
+
+let formulario = document.querySelector("#formulario");
+
+inputNombre.oninput = function (e) {
+  divNombre.innerText = e.target.value;
+};
+
+inputNumero.oninput = function (e) {
+  divNumero.innerText = e.target.value;
+};
+
+formulario.onsubmit = function (e) {
+  e.preventDefault();
+
+  alert(validator.isValid(inputNumero.value) ? "Es valido" : "No es valido");
+};
